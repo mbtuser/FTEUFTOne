a111d856993bd612de5ccec724ac8c77f2415011
mbtuser <94625165+mbtuser@users.noreply.github.com>
2026-04-23 15:39:05 +0300

    new test

---
+++ python_uft/Res1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\rkarat\\OneDrive - OpenText\\Documents\\GitHub\\FTEUFTOne\\python_uft\\Res1',namePairs:[]}]};
